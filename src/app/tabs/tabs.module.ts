55ab5f5ffe40501b75c22bb7867aef4b9f780d5b
SakakiKomachi <dewadevilzdewa@gmail.com>
2023-07-11 00:27:13 +0700

    project kelompok

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -2,18 +2,24 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
-  ],
+    HttpClientModule,
+    RouterModule.forChild([{ path: '', component: TabsPage }]),
+  ],  
   declarations: [TabsPage]
 })
+
+
 export class TabsPageModule {}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,59 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  leagues: any[] = [];
+  englishTeams: any[] = [];
+  spanishTeams: any[] = [];
+  latestEvent: any;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.getLeagues();
+    this.getEnglishTeams();
+    this.getSpanishTeams();
+    this.getLatestEvent();
+  }
 
+  getLeagues() {
+    this.http
+      .get('https://www.thesportsdb.com/api/v1/json/3/all_leagues.php')
+      .subscribe((response: any) => {
+        this.leagues = response.leagues;
+      });
+  }
+
+  getEnglishTeams() {
+    this.http
+      .get(
+        'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?l=English%20Premier%20League'
+      )
+      .subscribe((response: any) => {
+        this.englishTeams = response.teams;
+      });
+  }
+
+  getSpanishTeams() {
+    this.http
+      .get(
+        'https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?s=Soccer&c=Spain'
+      )
+      .subscribe((response: any) => {
+        this.spanishTeams = response.teams;
+      });
+  }
+
+  getLatestEvent() {
+    this.http
+      .get(
+        'https://www.thesportsdb.com/api/v1/json/3/eventslast.php?id=133602'
+      )
+      .subscribe((response: any) => {
+        this.latestEvent = response.results[0];
+      });
+  }
 }
